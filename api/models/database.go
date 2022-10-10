a616f4c2e1bafcb8b532a47668f2d7408faf2088
Your Name <you@example.com>
2022-10-10 18:59:57 +0300

    favs added

--- api/models/database.go
+++ api/models/database.go
@@ -7,28 +7,19 @@ import (
 	_ "github.com/lib/pq"
 )
 
-/*
 const (
 	host     = "ec2-99-81-16-126.eu-west-1.compute.amazonaws.com"
 	port     = 5432
 	user     = "ywmpggsikbsbqm"
 	password = "dc9395dcf455a164c32173b40c82ca99835ae28d371b7977b9b122c272be51bc"
 	dbname   = "d6anbpmcouj6s2"
-)*/
-
-const (
-	host     = "localhost"
-	port     = 5432
-	user     = "postgres"
-	password = "uyumak"
-	dbname   = "blogapp"
 )
 
 var db *gorm.DB
 
 func init() {
 	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
+		"password=%s dbname=%s sslmode=require",
 		host, port, user, password, dbname)
 
 	fmt.Println(psqlInfo)
