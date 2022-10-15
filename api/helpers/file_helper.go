80b06fb8cabb3bfe7ec878259fe7b18f231329cb
Your Name <you@example.com>
2022-10-15 11:15:43 +0300

    ha

--- api/helpers/file_helper.go
+++ api/helpers/file_helper.go
@@ -13,7 +13,7 @@ func ImageUploadHelper(input interface{}) (string, error) {
 	defer cancel()
 
 	cld, _ := cloudinary.NewFromParams("ddeatrwxs", "641461484529384", "Pg62Uh1szh18LjcCjCHA14z_oA8")
-	uploadParam, err := cld.Upload.Upload(ctx, input, uploader.UploadParams{Folder: "studapp",  Transformation: "q_50" })
+	uploadParam, err := cld.Upload.Upload(ctx, input, uploader.UploadParams{Folder: "studapp",  Transformation: "q_30" })
 	if err != nil {
 		return "", err
 	}
