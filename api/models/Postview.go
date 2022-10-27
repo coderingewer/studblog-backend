5aafeef321c3749c5f88788c879214885029c6e4
Your Name <you@example.com>
2022-10-27 10:04:06 +0300

    non

--- api/models/Postview.go
+++ api/models/Postview.go
@@ -15,3 +15,20 @@ func (view *View) ViewPost(pid uint) error {
 	}
 	return nil
 }
+
+func (view *View) DeleteViews(pid uint) error {
+	views := []View{}
+	err := GetDB().Debug().Table("views").Where("post_id", pid).Find(views).Error
+	if err != nil {
+		return err
+	}
+	if len(views) > 0 {
+		for i, _ := range views {
+			db := GetDB().Debug().Table("views").Where("id=? ", views[i].ID).Take(&views[i]).Delete(&View{})
+			if db.Error != nil {
+				return db.Error
+			}
+		}
+	}
+	return nil
+}
