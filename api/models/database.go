8f666cc6fe9c95684bcedbdf0355df64efafd10d
coderingewer <Yatli5645@gmail.com>
2022-12-15 21:14:42 +0300

    .

--- api/models/database.go
+++ api/models/database.go
@@ -8,11 +8,11 @@ import (
 )
 
 const (
-	host     = "ec2-99-81-16-126.eu-west-1.compute.amazonaws.com"
+	host     = "dpg-cedm5e02i3mr7lhduv20-a"
 	port     = 5432
-	user     = "ywmpggsikbsbqm"
-	password = "dc9395dcf455a164c32173b40c82ca99835ae28d371b7977b9b122c272be51bc"
-	dbname   = "d6anbpmcouj6s2"
+	user     = "blogapp_user"
+	password = "Ts9mtd9oFR9F8dWVEsEyT7fByFYngFLT"
+	dbname   = "blogapp"
 )
 
 var db *gorm.DB
